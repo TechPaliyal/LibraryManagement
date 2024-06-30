a64eca9fe5d57d865c54f62901a374d7a2e595b6
umesh choudhary paliyal <paliyalumesh@gmail.com>
2024-06-30 23:24:22 +0530

    LoginForm

--- src/Componants/LoginForm/LoginForm.tsx
+++ src/Componants/LoginForm/LoginForm.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import './LoginForm.css'
+import { FaUser ,FaLock } from "react-icons/fa";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Input } from "@/components/ui/input"
+
+
+
+
 
 function LoginForm() {
+
+    const { loginWithRedirect } = useAuth0();
+
+
+    
   return (
-    <div>LoginForm</div>
+
+    <div className='page'>
+        <div className='wallpaper'>
+            huiu
+        </div>
+    <div className='wrapper'>
+         <form className='form'>
+         <h1 id='logintext'>LogIn</h1>
+                <div className='input-cover'>
+            <div className='inputbox'>
+            <Input type="email" placeholder="Email" />
+                <FaUser className='icon'/>
+            </div>
+            <div className='inputbox'>
+                <input type="password" placeholder='Password' className='pwd' />
+                <FaLock className='icon' />
+            </div>
+                </div>
+            <div className='remember-forget'>
+                <label >  <input type="checkbox"/>Remember Me</label>
+                <a href="#">Forgot password?</a>
+            </div>
+        
+                <button onClick={() => loginWithRedirect()} className='login-btn'>Login</button>
+            
+            <p className='Or-text'> Or Login with</p>
+            <div className='Google-login'>
+                <button className='google-btn'><img className = 'icon-image' src="images/Google.jpg" alt="" />Google</button>
+            </div>
+            <div className='register'>
+                want to register? <a href="#">Register</a>
+            </div>
+         </form>
+
+    </div>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain="dev-1prw0ebk7dps1wbj.us.auth0.com"
+    clientId="mhqpHw7Wn6biOx16hB96FgFnhMmwOrhx"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+     <App />
+  </Auth0Provider>
+ 
+    
+  
 )
