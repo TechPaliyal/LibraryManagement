746c27d1edefd66c2714ca8a020751a48c3cfd3f
BP <141549205+bhagirathpaliyal@users.noreply.github.com>
2024-07-01 10:25:26 +0530

    Added React Router For Navigation (#22)
    * Added React Router For Navigation
    
    * fix : routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage'
+import NotFound from './NotFound'
 
 function App() {
  
 
   return (
     <>
-     <LandingPage/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path='LibraryManagement' element={<LandingPage/>}/>
+      <Route path='LibraryManagement/management/login' element={<LandingPage/>}/>
+      <Route path='LibraryManagement/user/login' element={<LandingPage/>}/>
+     
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
