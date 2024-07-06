3d9dfe9bc34bef288e2016175fccd74ee511a4c4
UmeshChoudhary <167175832+UmeshPaliyaal@users.noreply.github.com>
2024-07-06 18:40:49 +0530

    Quote (#24)
    * quote added
    
    * quote file added differently
    
    * path changed
    
    * css file deleted along with 2nd quote.tsx
    
    * format adjusted

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,17 +1,23 @@
-import { Button } from "@/components/ui/button"
-import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center">
-        <h1 className="text-[128px] font-medium-">LibraryHub</h1>
-        <p className="text-[36px] font-medium">Your central place for all library needs</p>
-        <div className="flex gap-[20px] mt-[50px]">
-      <Link to={'management/login'}><Button variant="outline">Create A Library</Button></Link>  
-       <Link to={'user/login'}><Button >Join A Library</Button></Link> 
-        </div>
+      <h1 className="text-[128px] font-medium-">LibraryHub</h1>
+      <p className="text-[36px] font-medium">
+        Your central place for all library needs
+      </p>
+      <div className="flex gap-[20px] mt-[50px]">
+        <Link to={"management/login"}>
+          <Button variant="outline">Create A Library</Button>
+        </Link>
+        <Link to={"management/login"}>
+          <Button>Join A Library</Button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
