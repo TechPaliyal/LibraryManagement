0d41fb3c6d6b6e73dca0cfa1c3b40c20f17e1deb
umesh choudhary paliyal <paliyalumesh@gmail.com>
2024-07-06 18:39:25 +0530

    format adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './Pages/LandingPage'
-import NotFound from './NotFound'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./Pages/LandingPage";
+import NotFound from "./NotFound";
 
 function App() {
- 
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path='LibraryManagement' element={<LandingPage/>}/>
-      <Route path='LibraryManagement/management/login' element={<LandingPage/>}/>
-      <Route path='LibraryManagement/user/login' element={<LandingPage/>}/>
-
-      
-     
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="LibraryManagement" element={<LandingPage />} />
+          <Route
+            path="LibraryManagement/management/login"
+            element={<LandingPage />}
+          />
+          <Route
+            path="LibraryManagement/user/login"
+            element={<LandingPage />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/mycomponents/Quote.tsx
+++ src/mycomponents/Quote.tsx
@@ -1,48 +1,54 @@
-import { useState,useEffect } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { useState, useEffect } from "react";
+import { twMerge } from "tailwind-merge";
 function Quote() {
-    const [Quote , setQuote] = useState("");
-    const [Author, setAuthor] = useState("");
+  const [Quote, setQuote] = useState("");
+  const [Author, setAuthor] = useState("");
 
-const api_url:string = "https://api.quotable.io/random";
-useEffect(()=>{
-async function getQuote(url:string){
-const response = await fetch(url);
-var data = await response.json();
-setQuote(data.content)
-setAuthor(data.author)
+  const api_url: string = "https://api.quotable.io/random";
+  useEffect(() => {
+    async function getQuote(url: string) {
+      const response = await fetch(url);
+      var data = await response.json();
+      setQuote(data.content);
+      setAuthor(data.author);
 
-localStorage.setItem('quote', data.content);
-localStorage.setItem('author' , data.author);
-localStorage.setItem('timestamp', new Date().toString());
-}
+      localStorage.setItem("quote", data.content);
+      localStorage.setItem("author", data.author);
+      localStorage.setItem("timestamp", new Date().toString());
+    }
 
-const storedQuote = localStorage.getItem('quote');
-const storedAuthor = localStorage.getItem('author');
-const storedTimeStamp = localStorage.getItem('timestamp');
+    const storedQuote = localStorage.getItem("quote");
+    const storedAuthor = localStorage.getItem("author");
+    const storedTimeStamp = localStorage.getItem("timestamp");
 
-if(storedQuote && storedAuthor && storedTimeStamp){
-    const currentTime = new Date();
-    const quoteTime = new Date(storedTimeStamp);
-    const hoursDifference = Math.abs(currentTime.getTime()-quoteTime.getTime())/36e5;
+    if (storedQuote && storedAuthor && storedTimeStamp) {
+      const currentTime = new Date();
+      const quoteTime = new Date(storedTimeStamp);
+      const hoursDifference =
+        Math.abs(currentTime.getTime() - quoteTime.getTime()) / 36e5;
 
-
-    if (hoursDifference <24){
+      if (hoursDifference < 24) {
         setAuthor(storedAuthor);
         setQuote(storedQuote);
         return;
+      }
     }
-}
 
-
-getQuote(api_url)
-}, []);
+    getQuote(api_url);
+  }, []);
   return (
-<div className={twMerge('flex-col bg-zinc-800 text-white w-1/2 h-full')}>
-<h1 className={twMerge('text-6xl font-medium text-left my-16 ml-28')}>Quote of the day</h1>
-<h1 className={twMerge('w-96 font-medium text-3xl ml-40 ')} >{`"${Quote}"`}</h1>
-<p className={twMerge(' font-medium text-2xl ml-96 my-16')}>{`author- "${Author}"`}</p>
-</div>  )
+    <div className={twMerge("flex-col bg-zinc-800 text-white w-1/2 h-full")}>
+      <h1 className={twMerge("text-6xl font-medium text-left my-16 ml-28")}>
+        Quote of the day
+      </h1>
+      <h1
+        className={twMerge("w-96 font-medium text-3xl ml-40 ")}
+      >{`"${Quote}"`}</h1>
+      <p
+        className={twMerge(" font-medium text-2xl ml-96 my-16")}
+      >{`author- "${Author}"`}</p>
+    </div>
+  );
 }
 
-export default Quote
+export default Quote;
