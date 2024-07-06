bb6b14f52d1f8406656d8aa674a1b9330b6882bb
umesh choudhary paliyal <paliyalumesh@gmail.com>
2024-07-06 19:03:07 +0530

    my branch mereger

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,34 +2,17 @@ import './LoginForm.css'
 import { FaUser ,FaLock } from "react-icons/fa";
 import { Input } from "@/components/ui/input"
 import { Button} from "@/components/ui/button"
-import { useState , useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import Quote from '@/mycomponents/Quote';
 
 function LoginForm() {
-
-const [Quote , setQuote] = useState("");
-const [Author, setAuthor] = useState("");
-
-const api_url:string = "https://api.quotable.io/random";
-useEffect(()=>{
-async function getQuote(url:string){
-const response = await fetch(url);
-var data = await response.json();
-setQuote(data.content)
-setAuthor(data.author)
-}
-getQuote(api_url)
-}, []);
-
 
 return (
 
 <div className='page'>
 <div className='wallpaper'>
 <div className='overlay'>
-<h1 id='line1'>Quote of the day</h1>
-<h1 id='line2'>{`"${Quote}"`}</h1>
-<p id='line3'>{`author- "${Author}"`}</p>
+<Quote/>
 </div>
 </div>
 <div className='wrapper'>
